2411d7b48c3b9becff9208725486ebb27f012665
colitas-K <chinakws0403@gmail.com>
2025-01-08 18:43:05 +0800

    Create 0015. 3Sum.rs

---
+++ 1_To_100/0015. 3Sum.rs
@@ -0,0 +1,45 @@
+impl Solution {
+    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        if nums.len() < 3 {
+            return vec![];
+        }
+
+        let mut result: Vec<Vec<i32>> = vec![];
+        let mut nums = nums;
+        nums.sort(); 
+
+        for i in 0..nums.len() - 2 {
+            if nums[i] > 0 {
+                break;
+            }
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue;
+            }
+
+            let mut left = i + 1;
+            let mut right = nums.len() - 1;
+
+            while left < right {
+                let sum = nums[i] + nums[left] + nums[right];
+                
+                if sum == 0 {
+                    result.push(vec![nums[i], nums[left], nums[right]]);
+                    
+                    while left < right && nums[left] == nums[left + 1] {
+                        left += 1;
+                    }
+                    while left < right && nums[right] == nums[right - 1] {
+                        right -= 1;
+                    }
+                    left += 1;
+                    right -= 1;
+                } else if sum < 0 {
+                    left += 1;
+                } else {
+                    right -= 1;
+                }
+            }
+        }
+        result
+    }
+}
