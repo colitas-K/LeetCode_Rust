27620ff5792e81f9997d753a6727a0da039e0ec3
colitas-K <84260914+colitas-K@users.noreply.github.com>
2025-01-20 00:27:55 +0800

    Update 0153. Find Minimum in Rotated Sorted Array.rs

--- 101_To_200/0153. Find Minimum in Rotated Sorted Array.rs
+++ 101_To_200/0153. Find Minimum in Rotated Sorted Array.rs
@@ -1,1 +1,23 @@
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() as usize - 1;
 
+        if nums[left] < nums[right] {
+            return nums[left];
+        }
+        
+        let mut min = nums[right];
+        
+        while left <= right {
+            let mid = (left + right) / 2;
+            if nums[mid] < min {
+                min = nums[mid];
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        min
+    }
+}
