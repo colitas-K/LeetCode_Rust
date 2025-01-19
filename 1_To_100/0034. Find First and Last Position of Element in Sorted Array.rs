518aacf03f15ccaffb6fee82816223f7cd47699f
colitas-K <84260914+colitas-K@users.noreply.github.com>
2025-01-20 00:14:03 +0800

    Update 0034. Find First and Last Position of Element in Sorted Array.rs

--- 1_To_100/0034. Find First and Last Position of Element in Sorted Array.rs
+++ 1_To_100/0034. Find First and Last Position of Element in Sorted Array.rs
@@ -1,19 +1,37 @@
 impl Solution {
     pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let len = nums.len();
-        let mut result: Vec<i32> = vec![-1, -1];
-        let mut num1 = 0;
-        let mut num2 = 0;
-        for i in 0..len {
-            if nums[i] == target && num1 == 0 {
-                result[0] = i as i32;
-                num1 += 1;
-            }
-            if nums[len - 1 - i] == target && num2 == 0 {
-                result[1] = (len - 1 - i) as i32;
-                num2 += 1;
+        let mut left = 0;
+        let mut right = nums.len() as i32 - 1 ;
+        let mut result_left = -1;
+        let mut result_right = -1;
+
+        while left <= right {
+            let mid = (left + right) / 2;
+            if nums[mid as usize] < target {
+                left = mid + 1;
+            } else if nums[mid as usize] > target {
+                right = mid - 1;
+            } else {
+                result_left = mid;
+                right = mid - 1;
             }
         }
-        result
+
+        let mut left = 0;
+        let mut right = nums.len() as i32 - 1 ;
+
+        while left <= right {
+            let mid = (left + right) / 2;
+            if nums[mid as usize] < target {
+                left = mid + 1;
+            } else if nums[mid as usize] > target {
+                right = mid - 1;
+            } else {
+                result_right = mid;
+                left = mid + 1;
+            }
+        }
+
+        vec![result_left, result_right]
     }
 }
