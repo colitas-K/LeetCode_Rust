a8c2aab5abb31905a11650acec954ad435c3861e
colitas-K <84260914+colitas-K@users.noreply.github.com>
2025-05-13 14:27:36 +0800

    Update 0003. Longest Substring Without Repeating Characters.rs

--- 1_To_100/0003. Longest Substring Without Repeating Characters.rs
+++ 1_To_100/0003. Longest Substring Without Repeating Characters.rs
@@ -2,17 +2,18 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        let mut longest_hash: HashMap<char, i32> = HashMap::new();
         let mut max = 0;
+        let mut substring: HashMap<char, usize> = HashMap::new();
         let mut left = 0;
 
-        for (right, letter) in s.chars().enumerate() {
-            if let Some(&index) = longest_hash.get(&letter) {
+        for (right,letter) in s.chars().enumerate() {
+            if let Some(&index) = substring.get(&letter) {
                 left = left.max(index + 1);
             }
-            longest_hash.insert(letter, right as i32);
-            max = max.max(right as i32 - left + 1);
+            substring.insert(letter, right);
+            max = max.max(right - left + 1);
         }
-        max
+
+        max as i32
     }
 }
